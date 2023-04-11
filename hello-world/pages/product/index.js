65c0f241a05d2127ba82cf4b62bbd57f2aeb2721
Efoneli <abbaefoneli@gmail.com>
2023-04-11 10:50:27 +0100

    dynamic routing

---
+++ hello-world/pages/product/index.js
@@ -0,0 +1,13 @@
+
+function Product() {
+  return (
+    <>
+        <h1>all products</h1>
+        <li>product 1</li>
+        <li>product 2</li>
+        <li>product 3</li>
+    </>
+  )
+}
+
+export default Product
