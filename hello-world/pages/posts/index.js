3e67616f2140c9cef919dbc9ce35797f48abaf5e
Efoneli <abbaefoneli@gmail.com>
2023-04-12 13:52:01 +0100

    get static

--- hello-world/pages/posts/index.js
+++ hello-world/pages/posts/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 function PostList({ posts }) {
     return (
         <>
@@ -6,9 +8,11 @@ function PostList({ posts }) {
                 posts.map((post) => {
                     return (
                         <div key={post.id}>
+                            <Link href={`posts/${post.id}`} passHref>
                             <h2>
-                                {post.id} {post.title}
+                                {post.id} {post.title}s
                             </h2>
+                            </Link>
                             <hr/>
                         </div>
                     )
@@ -23,7 +27,7 @@ export default PostList
 export async function getStaticProps() {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json()
-    
+
     return {
         props: {
             posts: data.slice(0,3)
