79b9feeb4973ed112ba3d61f20c9d765ba0dbfb1
Efoneli <abbaefoneli@gmail.com>
2023-04-13 12:03:36 +0100

    static

--- hello-world/pages/goods/[goodsId].js
+++ hello-world/pages/goods/[goodsId].js
@@ -1,10 +1,41 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 
-function GoodsId() {
-    const router = useRouter()
-    const goodId = router.query.goodsId
+function GoodsId( { users }) {
+    // const router = useRouter()
+    // const goodId = router.query.goodsId
 
-    return <h1>items here {goodId} </h1>
+    return 
+        <>
+            <h1>List of users</h1>
+            {users.map((user) => {
+                return (
+                    <div key={user.id}>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                    </div>
+                )
+            })}
+        </>
+
+    // <>
+    // <h1>
+    // {post.id} {post.title }
+    // </h1>
+    // <p>{post.body}</p>
+    // </>
+    // <h1>items here  </h1>
 }
 
 export default GoodsId
+
+export async function getStaticProps() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        props: {
+            users: data,
+        }
+    }
+}
